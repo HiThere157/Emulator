51282d7aec9393167c7564ad99af8e4a9bee9d07
David <50298235+HiThere157@users.noreply.github.com>
2023-03-10 18:54:18 +0100

    validation & sanitation

--- app/api/rom/[core]/[fileName]/route.ts
+++ app/api/rom/[core]/[fileName]/route.ts
@@ -1,21 +1,47 @@
 import path from "path";
 import { promises as fs } from "fs";
 
+import { cores } from "@/config/cores";
+
 export const revalidate = 0;
 
-type Props = {
-  params: {
-    core: string;
-    fileName: string;
-  };
+type FileProps = {
+  core: string;
+  fileName: string;
 };
-export async function GET(request: Request, { params }: Props) {
-  const romPath = path.join(process.cwd(), `data/roms/${params.core}/${params.fileName}`);
-  return new Response(await fs.readFile(romPath));
+
+function getRomPath(params: FileProps) {
+  const unsafePath = `data/roms/${params.core}/${params.fileName}`;
+  const safePath = path.normalize(unsafePath).replace(/^(\.\.(\/|\\|$))+/, "");
+  return path.join(process.cwd(), safePath);
 }
 
-export async function POST(request: Request, { params }: Props) {
-  const romPath = path.join(process.cwd(), `data/roms/${params.core}/${params.fileName}`);
-  await fs.writeFile(romPath, new Uint8Array(await request.arrayBuffer()));
-  return new Response();
+export async function GET(_request: Request, { params }: { params: FileProps }) {
+  const fileBlob = await fs.readFile(getRomPath(params));
+  return new Response(fileBlob);
+}
+
+export async function POST(request: Request, { params }: { params: FileProps }) {
+  const romPath = getRomPath(params);
+  const fileBuffer = new Uint8Array(await request.arrayBuffer());
+
+  // check core
+  if (!Object.keys(cores).includes(params.core)) {
+    return new Response(null, { status: 400 });
+  }
+
+  // path traversal should not be possible, since the core is whitelisted
+  try {
+    await fs.access(`data/roms/${params.core}`, fs.constants.F_OK);
+  } catch {
+    await fs.mkdir(`data/roms/${params.core}`);
+  }
+
+  try {
+    await fs.access(romPath, fs.constants.F_OK);
+    return new Response(null, { status: 400 });
+  } catch {
+    await fs.writeFile(romPath, fileBuffer);
+    return new Response();
+  }
 }
--- app/api/roms/route.ts
+++ app/api/roms/route.ts
@@ -1,19 +1,37 @@
 import path from "path";
 import { promises as fs } from "fs";
 
+import { cores } from "@/config/cores";
+
 export const revalidate = 0;
 
-export async function GET() {
+async function getDB() {
   const romDBPath = path.join(process.cwd(), "data/roms.json");
   const romDB: RomFile[] = JSON.parse(await fs.readFile(romDBPath, { encoding: "utf-8" }));
+  return { romDBPath, romDB };
+}
+
+export async function GET() {
+  const { romDB } = await getDB();
   return new Response(JSON.stringify(romDB));
 }
 
 export async function POST(request: Request) {
-  const romDBPath = path.join(process.cwd(), "data/roms.json");
-  const romDB: RomFile[] = JSON.parse(await fs.readFile(romDBPath, { encoding: "utf-8" }));
-
+  const { romDB, romDBPath } = await getDB();
   const newRom: RomFile = await request.json();
+
+  // basic input check
+  // check for duplicates
+  if (
+    !(newRom.friendlyName && newRom.fileName) ||
+    !Object.keys(cores).includes(newRom.core) ||
+    romDB.some((romFile) => {
+      return romFile.fileName === newRom.fileName && romFile.core === newRom.core;
+    })
+  ) {
+    return new Response(null, { status: 400 });
+  }
+
   romDB.push(newRom);
 
   await fs.writeFile(romDBPath, JSON.stringify(romDB), { encoding: "utf-8" });
